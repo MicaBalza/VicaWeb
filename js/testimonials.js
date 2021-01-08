3798331db00c6e52e9cb3f0df1058e1f9bc3d152
MicaBalza <mica.balza@gmail.com>
2021-01-08 05:07:57 -0300

    testimonials animation init

---
+++ js/testimonials.js
@@ -0,0 +1,11 @@
+const testimonial1 = document.querySelector("#testimonial-1");
+const testimonial2 = document.querySelector("#testimonial-2");
+const testimonial3 = document.querySelector("#testimonial-3");
+const indicator = document.querySelector("#indicator");
+var scrollLeft;
+/* 
+testimonial1.addEventListener("scroll", showNext());
+
+function showNext(e) {
+  console.log(e);
+} */
