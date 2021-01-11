203577990698eb4aaca26c5bc3e1138e9ea89ac3
MicaBalza <mica.balza@gmail.com>
2021-01-11 02:47:12 -0300

    whatsapp button test 1

--- js/whatsapp.js
+++ js/whatsapp.js
@@ -1,10 +1,15 @@
 whatsappButton = document.querySelector(".whatsapp");
 footerHeight = document.querySelector("footer").scrollHeight;
-var limit =
-  document.body.clientHeight -
-  window.innerHeight -
-  footerHeight +
-  whatsappButton.scrollHeight / 2;
+var height = Math.max(
+  document.body.scrollHeight,
+  document.body.offsetHeight,
+  document.documentElement.clientHeight,
+  document.documentElement.scrollHeight,
+  document.documentElement.offsetHeight
+);
+var limit = height + footerHeight - whatsappButton.scrollHeight / 2;
+
+/* height - window.innerHeight - footerHeight + whatsappButton.scrollHeight / 2; */
 
 var ticking = false;
 
@@ -25,7 +30,7 @@ window.onscroll = function (e) {
     } else if (scrollY > limit) {
       whatsappButton.style.position = "absolute";
       whatsappButton.style.bottom = `${-limit}px`;
-      console.log("aca");
+      console.log(limit);
       ticking = false;
     }
   }
